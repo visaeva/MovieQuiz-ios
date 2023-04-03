a02d51237a47c030b8f1b559632089827eefd9ec
Victoria Isaeva <visaeva0302@gmail.com>
2023-04-03 21:09:13 +0300

    Исправлены замечания, добавлено isEnabled для button

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,6 +9,7 @@ final class MovieQuizViewController: UIViewController {
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
+    
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
@@ -21,16 +22,19 @@ final class MovieQuizViewController: UIViewController {
     
     @IBOutlet private weak var counterLabel: UILabel!
     
+    @IBOutlet private weak var noClickedButton: UIButton!
     
-    // переменная с индексом текущего вопроса, начальное значение 0
+    @IBOutlet private weak var yesClickedButton: UIButton!
+    
     private var currentQuestionIndex = 0
     
-    // переменная со счётчиком правильных ответов
     private var correctAnswers = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.layer.cornerRadius = 20 //скругление рамки
+        
         let currentQuestion = questions[currentQuestionIndex]
         show(quiz: convert(model: currentQuestion))
     }
@@ -48,7 +52,7 @@ final class MovieQuizViewController: UIViewController {
         let questionNumber: String
     }
     
-    // "Результат квиза"
+    
     struct QuizResultsViewModel {
         let title: String
         let text: String
@@ -106,9 +110,8 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    // приватный метод, который содержит логику перехода в один из сценариев
+    
     private func showNextQuestionOrResults() {
-        view.isUserInteractionEnabled = true
         
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers)/10"
@@ -129,20 +132,26 @@ final class MovieQuizViewController: UIViewController {
             show(quiz: viewModel)
         }
     }
-    // приватный метод, который меняет цвет рамки
+    
+    // меняем цвет рамки
     private func showAnswerResult(isCorrect: Bool) {
+        yesClickedButton.isEnabled = false
+        noClickedButton.isEnabled = false
+        
         if isCorrect {
             correctAnswers += 1
         }
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        imageView.layer.cornerRadius = 20 //скругление рамки
         
-        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        
+        // запускаем задачу через 1 сек
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.showNextQuestionOrResults()
             self.imageView.layer.borderWidth = 0
+            self.yesClickedButton.isEnabled = true
+            self.noClickedButton.isEnabled = true
+            self.showNextQuestionOrResults()
         }
     }
 }
